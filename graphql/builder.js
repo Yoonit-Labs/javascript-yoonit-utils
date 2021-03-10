3f57bbf1bb58feb7f700461a6fcaa8cd2ac4c4eb
Gabrie Moraes <moraes.madm@gmail.com>
2021-03-09 21:17:28 -0300

    Adds jsdocs to builder.js

--- graphql/builder.js
+++ graphql/builder.js
@@ -1,5 +1,10 @@
 
-// this function parses the arrays on mutations arguments so graphql doesnt fuck up
+/*
+ *  Parses arguments on query and mutation for graphql
+ *  @method parseToGql
+ *  @param  {Array | Object | Boolean} Receive an array, object or a boolean to parse
+ *  @return {String} Returns the parsed parameter
+ */
 const parseToGql = args => {
   let newArgs = JSON.stringify(args)
   newArgs = newArgs.replace(/\"([A-z])\w+\"(\:| \:)/g, elm => {
@@ -9,7 +14,16 @@ const parseToGql = args => {
   return newArgs
 }
 
-// this function constructs the query/mutation body calling other functions
+/*
+ *  The main function of the file. This function builds the query/mutation in parts and each part return another function
+ *  @method PromiseMaker
+ *  @param  {String} receive the mutation name
+ *  @return {Function} receive the arguments object from the previous function and call buildArgs to build it
+ *  @param  {Object} receive the arguments object. Receive it from {query | mutation} method on return
+ *  @return {Function} receive the expected fields array from the previous function and call buildRequetedFields to build it
+ *  @param  {Array} receive an array with the expected response fields. Receive it from {query | mutation} method on return
+ *  @return {String} builds a query/mutation string with all the parameters and returns it
+ */
 const construct = (type, methodName) => {
 
   return (args) => {
@@ -33,8 +47,14 @@ const construct = (type, methodName) => {
   }
 }
 
-// this is the main mutation function, it calls the construct function and its called by the user
-// this.mutation('endpoint')({arguments}, token)(requestedFields)
+/*
+ *  Receives all the params, verify if the mutationName exists and returns the next function on chain or false
+ *  @method mutation
+ *  @param  {String} receive the mutation name
+ *  @param  {Object} receive the arguments object. Pass it to construct on return.
+ *  @param  {Array} receive an array with the expected response fields. Pass it to construct on return. 
+ *  @return {Function | Boolean} construct | false
+ */
 export const mutation = (mutationName) => {
   if(!mutationName) {
     console.warn("You need to specify an endpoint")
@@ -47,8 +67,12 @@ export const mutation = (mutationName) => {
   return construct.call(this, 'mutation', mutationName)
 }
 
-// this is the main query function, it calls the construct function and its called by the user
-// this.query('endpoint')({arguments}, token)(requestedFields)
+/*
+ *  Receives all the params, verify if the queryName exists and returns the next function on chain or false
+ *  @method query
+ *  @param  {String} receive the query name
+ *  @return {Function | Boolean} construct | false
+ */
 export const query = (queryName) => {
   if(!queryName) {
     console.warn("You need to specify an endpoint")
@@ -61,12 +85,22 @@ export const query = (queryName) => {
   return construct.call(this, 'query', queryName)
 }
 
-// this function returns the requested fields string
+/*
+ *  Calls reduceFunction to construct the requested fields string
+ *  @method buildRequestedFields
+ *  @param  {Array} receive n params to pass through reduce function
+ *  @return {String} returns the result from reduce
+ */
 const buildRequestedFields = (fields) => {
   return fields.reduce((...args) => reduceFunction(...args))
 }
 
-// this function builds the requested fields string
+/*
+ *  Its called by buildRequestedFields, its the reduce function that it's used to build the requested fields string
+ *  @method reduceFunction
+ *  @param  {acc, elm} these parameters are given on reduce
+ *  @return {String}
+ */
 const reduceFunction = (acc, elm) => {
   if (typeof elm === 'string') {
     acc = `${acc}, ${elm}`
@@ -90,7 +124,12 @@ const reduceFunction = (acc, elm) => {
   return acc
 }
 
-// this function builds the arguments string
+/*
+ *  Builds the arguments string
+ *  @method buildArgs
+ *  @param  {Object} receive an object with the arguments
+ *  @return {String} returns the string to build the query/mutation
+ */
 const buildArgs = (args) => {
   if (!args || args.constructor !== Object) {
     return ''
